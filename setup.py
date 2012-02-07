53b9b0ebbd319626c9183791b525d347aa720e41
Bill Cauchois <wcauchois@gmail.com>
2012-02-07 10:46:46 -0800

    fix setup script

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ from distutils.core import setup, Extension
 from glob import glob
 
 lib_sources = glob('lib/*.c')
-lib_sources.remove('lib/svm_loqo.c') # this is an alternate backend for SVM-Light; only
-                                     # one of {svm_loqo.c,svm_hideo.c} may be compiled
-                                     # with this extension.
-lib_sources.remove('lib/svm_classify.c') # this file implements the "classify" binary;
-                                         # don't include it, since it defines main()
-                                         # again!
+try:
+  lib_sources.remove('lib/svm_loqo.c') # this is an alternate backend for SVM-Light; only
+                                       # one of {svm_loqo.c,svm_hideo.c} may be compiled
+                                       # with this extension.
+except ValueError:
+  pass
+try:
+  lib_sources.remove('lib/svm_classify.c') # this file implements the "classify" binary;
+                                           # don't include it, since it defines main()
+                                           # again!
+except ValueError:
+  pass
 
 setup(name         = 'svmlight',
       version      = '0.2',
