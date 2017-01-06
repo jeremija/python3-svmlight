563f80af815877c3826f01562347da97e3b2e33e
jeremija <jerko.steiner@gmail.com>
2017-01-06 11:28:03 -0500

    Update sample.py to work with python3

--- examples/simple.py
+++ examples/simple.py
@@ -6,12 +6,12 @@ test_data = __import__('data').test0
 # train a model based on the data
 model = svmlight.learn(training_data, type='classification', verbosity=0)
 
-# model data can be stored in the same format SVM-Light uses, for interoperability
-# with the binaries.
+# model data can be stored in the same format SVM-Light uses, for
+# interoperability with the binaries.
 svmlight.write_model(model, 'my_model.dat')
 
-# classify the test data. this function returns a list of numbers, which represent
-# the classifications.
+# classify the test data. this function returns a list of numbers, which
+# represent the classifications.
 predictions = svmlight.classify(model, test_data)
 for p in predictions:
-    print '%.8f' % p
+    print('%.8f' % p)
